7bea10b5534eabca6f5ab08abae3f14061e81922
SatyamMurari162 <satyam82873@gmail.com>
2026-08-28 20:05:51 +0530

    ArmstrongNumber Problem

--- Basic/Prb1-Armstrong/ArmstrongNumber.java
+++ Basic/Prb1-Armstrong/ArmstrongNumber.java
@@ -2,40 +2,31 @@ import java.util.Scanner;
 
 class ArmstrongNumber {
     public static void main(String[] args) {
-        int num = 0, res = 0, pow = 0, sum = 0;
+        int num = 0, res = 0, count = 0, sum = 0;
 
         System.out.print("Enter ArmstrongNumber: ");
         Scanner sc = new Scanner(System.in);
-
         num = sc.nextInt();
 
         int originalNum = num;
 
         while (num > 0) {
-            res = num % 10;
             num = num / 10;
-            pow++;
-            for(int i = 0; i<=pow; i++){
-                sum += res * pow;
-            }
+            count++;
         }
 
         num = originalNum;
 
         while(num>0){
             res = num % 10;
-
-            sum += Math.pow(res, pow);
-            
+            sum += (int) Math.pow(res, count);
             num = num /10;
         }
 
-        System.out.println(sum);
-
         if(originalNum == sum){
-            System.out.println(originalNum + " is an armstrongnumber");
+            System.out.println(originalNum + " " + "is an armstrongnumber");
         }else{
-            System.out.println(originalNum + " is not an armstrong number");
+            System.out.println(originalNum + " " + "is not an armstrong number");
         }
 
         sc.close();
