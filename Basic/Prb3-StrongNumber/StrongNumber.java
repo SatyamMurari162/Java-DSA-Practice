aa6cc967d79623bc58018715884aa9c362d97b1f
SatyamMurari162 <satyam82873@gmail.com>
2026-08-30 22:17:54 +0530

    Check Strong Number

---
+++ Basic/Prb3-StrongNumber/StrongNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class StrongNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
+
+        int originalNum = num;
+        int digits = 0, sum = 0;
+        while(num>0){
+            int fac = 1;
+            digits = num % 10;
+            for(int i = 1; i<=digits; i++){
+                fac *= i;
+            }
+            sum += fac;
+            num = num / 10;
+        }
+        
+        if(sum == originalNum){
+            System.out.println(originalNum + " " + "is a perfect number");
+        }else{
+            System.out.println(originalNum + " " + "is not a perfect number");
+        }
+
+        sc.close();
+    
+    }
+}
