6d47898e396d95b2b104c6c4a0a042b8a12bacdc
SatyamMurari162 <satyam82873@gmail.com>
2026-08-31 23:10:32 +0530

    Reverse-Number

---
+++ Basic/Prb4_ReverseNumber/ReverseNumber.java
@@ -0,0 +1,28 @@
+package Basic.Prb4_ReverseNumber;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String []args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
+        int rev = reverse(num);
+        System.out.println(rev);
+
+        sc.close();
+    }
+
+    public static int reverse(int n) {
+        int rev = 0;
+
+        while(n>0){
+            int digits = n % 10;
+            rev = rev * 10 + digits;
+            n = n / 10;
+        }
+
+        return rev;
+    }
+}
---
+++ Basic/Prb4_ReverseNumber/readme.md
@@ -0,0 +1,2 @@
+P4. Reverse the Digits of a Number
+Problem: Reverse the digits of the integer 8536 (output should be 6358).
