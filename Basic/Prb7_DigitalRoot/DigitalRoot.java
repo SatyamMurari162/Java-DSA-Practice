31a6cbc06ec7ae7d31dd75b793dedabc64b75ddd
SatyamMurari162 <satyam82873@gmail.com>
2026-09-03 23:57:06 +0530

    DigitalRoot Problem

--- Basic/Prb7_DigitalRoot/DigitalRoot.java
+++ Basic/Prb7_DigitalRoot/DigitalRoot.java
@@ -2,22 +2,43 @@ package Basic.Prb7_DigitalRoot;
 
 import java.util.Scanner;
 
-public class DigitalRoot {
+// Method 1 : Find Digital Root
+// public class DigitalRoot {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter Number: ");
+//         int num = sc.nextInt();
+
+//         while (num >= 10) {
+//             int sum = 0;
+//             while (num > 0) {
+//                 sum += num % 10;
+//                 num = num / 10;
+//             }
+//             num = sum;
+//         }
+
+//         System.out.println("Digital Root: "+ num);
+
+//         sc.close();
+//     }
+// }
+
+//Method 2
+
+public class DigitalRoot{
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number: ");
         int num = sc.nextInt();
 
-        while (num >= 10) {
-            int sum = 0;
-            while (num > 0) {
-                sum += num % 10;
-                num = num / 10;
-            }
-            num = sum;
+        if(num == 0){
+            System.out.println("Digital Root: "+ num);
+        }else{
+            int answer = 1 + (num-1) % 9;
+            System.out.println("Digital Root: " + answer);
         }
-
-        System.out.println("Digital Root: "+ num);
 
         sc.close();
     }
