f363b1e296e9782559668837d9bf14352e5d1d09
SatyamMurari162 <satyam82873@gmail.com>
2026-09-07 23:17:37 +0530

    Check Armstrong Number

---
+++ Basic/Prb1_CheckArmstrongNumber/Armstrong.java
@@ -0,0 +1,35 @@
+package Basic.Prb1_CheckArmstrongNumber;
+
+import java.util.Scanner;
+
+class Armstrong {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter NUmber: ");
+    int num = sc.nextInt();
+    int originalNum = num;
+
+    int pow = 0;
+    while(num > 0){
+      num = num / 10;
+      pow++;
+    }
+
+    num = originalNum;
+    int sum = 0;
+    while (num != 0) {
+      int digits = num % 10;
+      sum+= (int) Math.pow(digits, pow);
+      num = num / 10;
+    }
+
+    if(sum == originalNum){
+      System.out.println(originalNum + " is an armstrong number");
+    }else{
+      System.out.println(originalNum + " is  not an armstrong number");
+    }
+
+    sc.close();
+  }
+}
---
+++ Basic/Prb1_CheckArmstrongNumber/readme.md
@@ -0,0 +1,8 @@
+P1. Check Armstrong Number
+
+Problem: An Armstrong number is one where the sum of its own digits, each raised to the power of the number of
+digits, equals the number itself (e.g., 153 = 1³+5³+3³). Check whether 371 is an Armstrong number.
+
+Approach:
+Convert the number to a string to get its digit count, then sum each digit raised to that power. Compare the sum to the
+original number.
