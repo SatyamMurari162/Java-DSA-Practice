2f976fe2562ea560064ce5f96dc2472b71ca4c0f
SatyamMurari162 <satyam82873@gmail.com>
2026-09-17 22:58:34 +0530

    Right Angled Triangle

---
+++ Pattern_Printing/Prb1_RightAngledTrianle/RightAngled.java
@@ -0,0 +1,13 @@
+package Pattern_Printing.Prb1_RightAngledTrianle;
+
+public class RightAngled {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
