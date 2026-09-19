7b7e74c2ac1d71d7529fe07f3fc8a83defdb8cac
SatyamMurari162 <satyam82873@gmail.com>
2026-09-19 23:11:19 +0530

    PascalsTriangle

--- Pattern_Printing/Prb6_PascalsTriangle/PascalTriangle.java
+++ Pattern_Printing/Prb6_PascalsTriangle/PascalTriangle.java
@@ -2,6 +2,14 @@ package Pattern_Printing.Prb6_PascalsTriangle;
 
 public class PascalTriangle {
     public static void main(String[] args) {
-        
+        int n = 5;
+        for(int i = 0; i<n; i++){
+            int value = 1;
+            for(int j = 0; j<=i; j++){
+                System.out.print(value + " ");
+                value = value * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
     }
 }
