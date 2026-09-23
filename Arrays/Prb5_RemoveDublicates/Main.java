a8a9ef3917095e9bce481fd1208fa47c1b796a4a
SatyamMurari162 <satyam82873@gmail.com>
2026-09-23 23:47:53 +0530

    Remove Duplicates Elements

--- Arrays/Prb5_RemoveDublicates/Main.java
+++ Arrays/Prb5_RemoveDublicates/Main.java
@@ -1,10 +1,17 @@
 package Arrays.Prb5_RemoveDublicates;
 
-import java.util.Arrays;
+
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1,2,2,3,4,5};
-        System.out.println(Arrays.toString(arr));
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+
+        System.out.println(set);
     }
 }
