a01184cdbcb3482fd49b60e69b64c0f21e1b14a6
SatyamMurari162 <satyam82873@gmail.com>
2026-09-23 23:40:05 +0530

    Remove Duplicates Elements

--- Arrays/Prb5_RemoveDublicates/Main.java
+++ Arrays/Prb5_RemoveDublicates/Main.java
@@ -1,8 +1,10 @@
 package Arrays.Prb5_RemoveDublicates;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1,2,2,3,4,5};
-        
+        System.out.println(Arrays.toString(arr));
     }
 }
