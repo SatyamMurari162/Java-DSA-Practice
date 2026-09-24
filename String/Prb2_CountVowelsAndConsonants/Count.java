7b891a27d1c3206f4aa380e6913c50e101d35d15
SatyamMurari162 <satyam82873@gmail.com>
2026-09-24 23:30:47 +0530

    Count Vowels and Consonants

--- String/Prb2_CountVowelsAndConsonants/Count.java
+++ String/Prb2_CountVowelsAndConsonants/Count.java
@@ -10,7 +10,24 @@ public class Count {
         System.out.print("Enter string: ");
         String str = sc.nextLine();
 
-        System.out.println(str);
+        int vol = 0, con = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char) (ch + 32);
+            }
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vol++;
+                } else {
+                    con++;
+                }
+            }
+        }
+        System.out.println("Vowels: " + vol);
+        System.out.println("Consonents: " + con);
 
         sc.close();
     }
