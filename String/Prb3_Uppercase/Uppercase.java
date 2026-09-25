e6eb6ac2e55fa8a8bb0477fa5f8c0dd2dcb1f716
SatyamMurari162 <satyam82873@gmail.com>
2026-09-25 22:42:15 +0530

    Convert Lowercase to Uppercase

---
+++ String/Prb3_Uppercase/Uppercase.java
@@ -0,0 +1,24 @@
+package String.Prb3_Uppercase;
+
+import java.util.Scanner;
+
+public class Uppercase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = "";
+        for(int i = 0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch >= 'a' && ch <= 'z'){
+                ch = (char) (ch - 32);
+            }
+            result += ch;
+        }
+
+        System.out.println(result);
+        sc.close();
+    }
+}
