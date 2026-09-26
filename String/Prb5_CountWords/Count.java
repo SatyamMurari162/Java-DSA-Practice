57063ce82bd38d61ed4f401a7e248f8052f81c7a
SatyamMurari162 <satyam82873@gmail.com>
2026-09-26 21:20:49 +0530

    Count words in a sentence

--- String/Prb5_CountWords/Count.java
+++ String/Prb5_CountWords/Count.java
@@ -3,7 +3,19 @@ package String.Prb5_CountWords;
 public class Count {
     public static void main(String[] args) {
         String str = "The quick brown fox jumps over the lazy dog.";
-
-        System.out.println(str);
+        int count = 0;
+        boolean inWord = false;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch != ' ') {
+                if (!inWord) {
+                    count++;
+                    inWord = true;
+                }
+            } else {
+                inWord = false;
+            }
+        }
+        System.out.println(count);
     }
 }
